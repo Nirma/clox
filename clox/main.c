61b4156e092a0f3eafe5dfea123aefc64c407a49
Nicholas Maccharoli <nicko@screaming-cactus.com>
2023-02-22 00:23:18 +0900

    Initial Commit

---
+++ clox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  clox
+//
+//  Created by Nicholas Maccharoli on 2023/02/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
